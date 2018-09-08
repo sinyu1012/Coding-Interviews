7e37f118c9f4eb27303710a0b6d271b266af645e
sinyu <1341156974@qq.com>
2018-09-08 15:15:56 +0800

    两个栈实现队列

---
+++ src/Five两个栈实现队列/Test.java
@@ -0,0 +1,14 @@
+package Five两个栈实现队列;
+
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution s=new Solution();
+		for (int i = 0; i < 10; i++) {
+			s.push(i);
+		}
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+		
+	}
+}
